8f5d6dc597b6c3df8712c01bc44d6b5e21a1e186
nuneslg <lgnunees@gmail.com>
2023-09-27 12:57:19 -0300

    add: node structure

---
+++ src/node/Node.java
@@ -0,0 +1,24 @@
+package node;
+
+public class Node<E> {
+    public E element;
+    public Node<E> next;
+
+    /**
+     * Creates a node of a generic type
+     * @param element the element to be stored in the node
+     * @param next the next node in the list
+     */
+    public Node(E element, Node<E> next) {
+        this.element = element;
+        this.next = next;
+    }
+
+    /**
+     * Creates a node of a generic type. Used for the header node
+     * @param next the next node in the list
+     */
+    public Node(Node<E> next) {
+        this.next = next;
+    }
+}
