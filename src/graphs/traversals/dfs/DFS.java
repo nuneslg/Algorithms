97115b6fc1c7035b0f0db3cb36154a0df157f5f3
nuneslg <lgnunees@gmail.com>
2023-09-27 22:25:33 -0300

    refactor(traversals): change functional interface to abstract method

--- src/graphs/traversals/dfs/DFS.java
+++ src/graphs/traversals/dfs/DFS.java
@@ -5,12 +5,12 @@ import graphs.traversals.PosVisit;
 import graphs.traversals.PreVisit;
 import graphs.traversals.Traversal;
 
-public abstract class DFS extends Traversal implements PreVisit, PosVisit {
+public abstract class DFS<E> extends Traversal {
 
 
     protected abstract void preVisit(Graph g, int v);
 
-    protected abstract void postVisit(Graph g, int v);
+    protected abstract E postVisit(Graph g, int v);
 
     /**
      * Traverses a graph using depth-first search
@@ -22,7 +22,7 @@ public abstract class DFS extends Traversal implements PreVisit, PosVisit {
         preVisit(g, v);
         g.setMark(g,v, 1);
         int w = g.first(g,v);
-        while (w < g.getNumVertices(g)) {
+        while (w < g.numVertices) {
             if (g.getMark(g, w) == 0) {
                 traverse(g, w);
             }
