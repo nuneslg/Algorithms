f8ce3a8df7ac7db9262b5884a48a2fcc2d1effc7
nuneslg <lgnunees@gmail.com>
2023-10-02 17:56:07 -0300

    feat(heaps) add insert and removeTop methods

--- src/trees/heaps/Heap.java
+++ src/trees/heaps/Heap.java
@@ -6,6 +6,7 @@ public abstract class Heap<T> {
     public int size; // size of the heap
     public ArrayList<T> heap; // array representing the heap
 
+
     public Heap(ArrayList<T> array) {
         this.heap = new ArrayList<>();
         this.heap.add(null); // the first element of the array is null
@@ -16,10 +17,26 @@ public abstract class Heap<T> {
 
     /**
      * Heapifies the array
+     *
      * @param H the array
      * @param n the size of the array
      */
     public abstract void Heapify(ArrayList<T> H, int n);
+
+
+    public void insert(T element) {
+        this.heap.add(element);
+        this.size++;
+        Heapify(this.heap, this.size);
+    }
+
+    public T removeTop() {
+        T top = this.heap.get(1);
+        this.heap.remove(1);
+        this.size--;
+        Heapify(this.heap, this.size);
+        return top;
+    }
 }
 
-// public void removeTop()
+
